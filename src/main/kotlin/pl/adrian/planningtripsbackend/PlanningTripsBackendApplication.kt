c070e57b8e49a832df5c7d85d634bbb317c462cb
Adrian Sadurski <sadurskiadrian@interia.eu>
2021-09-19 13:49:31 +0200

    Initial commit

---
+++ src/main/kotlin/pl/adrian/planningtripsbackend/PlanningTripsBackendApplication.kt
@@ -0,0 +1,11 @@
+package pl.adrian.planningtripsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanningTripsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlanningTripsBackendApplication>(*args)
+}
---
+++ src/test/kotlin/pl/adrian/planningtripsbackend/PlanningTripsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.adrian.planningtripsbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanningTripsBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
